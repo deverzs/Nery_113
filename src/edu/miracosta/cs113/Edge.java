11e67dfa3c290c9c0cd30f3d227b157337bb8345
Zsuzsanna Dianovics <31230409+deverzs@users.noreply.github.com>
2018-12-06 15:58:27 -0800

    Update Edge.java
    Edge updated

--- src/edu/miracosta/cs113/Edge.java
+++ src/edu/miracosta/cs113/Edge.java
@@ -1,4 +1,7 @@
 package edu.miracosta.cs113;
+
+import java.util.Map;
+
 /**
  * Edge.java : Edge for graph data structure that tracks source, destination and weight . Based on code from Data Structures by
  * Koffman and Wolfgang
@@ -13,31 +16,79 @@ public class Edge {
     private int destination;
     private double weight;
 
+    /**
+     * Constructor for Edge
+     * @param source vertex for Edge
+     * @param destination  destination for vertex
+     */
     public Edge(int source, int destination){
         this.source = source;
         this.destination = destination;
         this.weight = 1.0;
     }
 
+    /**
+     * Full constructor for Edge
+     * @param source vertex for Edge
+     * @param destination destination for vertex
+     * @param weight the weight
+     */
     public Edge(int source, int destination, double weight){
         this.source = source;
         this.destination = destination;
         this.weight = weight;
     }
 
+    /**
+     * Return the source
+     * @return source
+     */
     public int getSource(){
-        return 0;
+        return this.source ;
     }
 
+    /**
+     * Return the destination
+     * @return destination
+     */
     public int getDestination(){
-        return 0;
+        return this.destination ;
     }
 
+    /**
+     * Returns the weight
+     * @return weight
+     */
     public double getWeight(){
-        return 0.0;
+        return this.weight ;
     }
 
+    @Override
     public String toString(){
         return "Source: " + this.source + "\nDestination: " + this.destination + "\nWeight: " + this.weight;
+    }
+
+    @Override
+    public  int hashCode() {
+        int sum = 0 ;
+        Integer sourceHash = new Integer(source) ;
+        Integer destHash = new Integer(destination) ;
+        sum += sourceHash.hashCode() + destHash.hashCode() ;
+        return sum ;
+
+    }
+
+    @Override
+    public boolean equals(Object o ) {
+        if (o == null ) {
+            return false ;
+        }
+        if (!(o instanceof Edge)) {
+            return false ;
+        }
+        Edge anotherEdge = (Edge) o ;
+        return ( (destination == anotherEdge.getDestination()) )&&
+                (source == anotherEdge.getSource())  ;
+
     }
 }

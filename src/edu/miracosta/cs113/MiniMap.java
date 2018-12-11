b93e695a73ccb6b58532136c6c9ab038469f4aae
Oscar Fernandez Paz <38364745+OscarCSI@users.noreply.github.com>
2018-12-10 19:29:06 -0800

    added MiniMap Constructor and getSHortcut method

--- src/edu/miracosta/cs113/MiniMap.java
+++ src/edu/miracosta/cs113/MiniMap.java
@@ -1,10 +1,43 @@
 package edu.miracosta.cs113;
 
+import org.w3c.dom.ls.LSException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MiniMap {
-    int source;
-    int destination;
-    DijkstraAlgorithm shortCut;
-    ListGraph<?> currentMap;
+    public static final String MAP_FILE = "localMap.txt";
+    public static final boolean DEFAULT_DIRECTED_CHOICE = true;
+    private int source;
+    private int destination;
+    private DijkstraAlgorithm shortCut;
+    private ListGraph<?> currentMap;
+
+//    public MiniMap(int source)
+
+    public MiniMap(int source, int destination){
+        this.currentMap = MiniMap.buildMap(MAP_FILE, DEFAULT_DIRECTED_CHOICE);
+        this.shortCut = new DijkstraAlgorithm(this.currentMap, source);
+
+    }
+
+    public String getShortestPath(){
+       return this.shortCut.findFromToEnd(this.destination);
+    }
+
+    public static <E> ListGraph<E> buildMap(String file, boolean isDirected){
+        Scanner inFile = null;
+        try{
+            inFile = new Scanner(new FileInputStream(file));
+        } catch (FileNotFoundException e){
+            System.out.println("Error Loading File");
+        }
+        ListGraph<E> temp = ListGraph.createGraph(inFile, isDirected);
+        return temp;
+    }
+
+    //return shortest path
 
 
 }
